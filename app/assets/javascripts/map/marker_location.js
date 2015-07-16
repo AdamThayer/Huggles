7a5dd36afc058107f86a7d71bf10ebb520e9bcbd
Adam <adamjthayer@gmail.com>
2015-07-16 13:00:35 -0700

    created navbar

--- app/assets/javascripts/map/marker_location.js
+++ app/assets/javascripts/map/marker_location.js
@@ -3,7 +3,8 @@ huggles.after_google = huggles.after_google || []
 
 huggles.after_google.push(
     function () {
-        google.maps.event.addListener(map, "rightclick", function(event) {
+
+        google.maps.event.addListener(map, 'click', function(event) {
             var lat = event.latLng.lat();
             var lng = event.latLng.lng();
             // populate yor box/field with lat, lng
